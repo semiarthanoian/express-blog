ee4b6af8ab41964a0caf85b0749b66e8572eeefb
Semi Art <thinhtran.nature@gmail.com>
2022-09-14 03:17:00 +0700

    update

--- test.js
+++ test.js
@@ -1,1 +1,29 @@
-// writing test here...
+const mysql = require("mysql");
+
+var instance = mysql.createConnection({
+    host: "localhost",
+    port: "3045",
+    user: "root",
+    password: "123456789"
+});
+
+instance.connect(async (error) => {
+    if (error != null)
+        throw error;
+    else
+        console.log("Connected");
+});
+
+var sql = "SELECT `id`, `name`, `district`, `population` "
+        + "FROM world.city "
+        + "ORDER BY `id` DESC "
+        + "LIMIT 10, 5 "
+        + "; -- SELECT"
+; // sql
+
+instance.query(sql, async (error, resultset) => {
+    if (error != null)
+        throw error;
+    else
+        console.table(resultset);
+});
